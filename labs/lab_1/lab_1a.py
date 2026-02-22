ad7656031e1c733817f62798aab4957afea21172
sahajp0618-pixel <sahajp0618@gmail.com>
2026-02-22 12:26:16 -0500

    Update lab_1a.py
    Short Hobby Description Added

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Sahaj" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("I like to play basketball with my friends and go biking on the weekends with my friends.")
 if __name__ == "__main__":
     main()
